c0c8d90eab57c6daa7819716eed78db0659ddbcc
fargojan <145560591+fargojan@users.noreply.github.com>
2023-09-20 14:03:25 +0300

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-ck5d1beg2bec738cj240-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "yYcIlNF3WuBWi3pZk3kf2LIPK4TyCTY1",
+    DB: "fargojandb",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
